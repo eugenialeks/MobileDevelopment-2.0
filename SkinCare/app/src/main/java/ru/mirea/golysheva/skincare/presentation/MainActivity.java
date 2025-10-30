1b115c813c5bf913115bf8271e28fa4ecab51023
Eugenia <185637443+eugenialeks@users.noreply.github.com>
2025-10-30 17:11:43 +0300

    SkinCare

--- SkinCare/app/src/main/java/ru/mirea/golysheva/skincare/presentation/MainActivity.java
+++ SkinCare/app/src/main/java/ru/mirea/golysheva/skincare/presentation/MainActivity.java
@@ -1,9 +1,15 @@
 package ru.mirea.golysheva.skincare.presentation;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import ru.mirea.golysheva.skincare.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +28,13 @@ public class MainActivity extends AppCompatActivity {
             } else if (id == R.id.nav_catalog) {
                 f = new CatalogFragment();
             } else if (id == R.id.nav_scan) {
-                f = new ScanFragment();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null && user.isAnonymous()) {
+                    Toast.makeText(this, "Сканирование доступно только авторизованным пользователям", Toast.LENGTH_SHORT).show();
+                    return false;
+                } else {
+                    f = new ScanFragment();
+                }
             } else if (id == R.id.nav_fav) {
                 f = new FavoritesFragment();
             } else if (id == R.id.nav_profile) {
